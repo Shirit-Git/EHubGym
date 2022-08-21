3ec1ee57092d42c59024471424800d8fccd11c22
Shristi Sunar <nsristi123@gmail.com>
2022-08-22 03:23:06 +0545

    Gym Update

---
+++ OnlineShop/Models/SpecialTag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Models
+{
+    public class SpecialTag
+    {
+        public int Id { get; set; }
+        [Display(Name = "Location")]
+        public string Name { get; set; }
+    }
+}
